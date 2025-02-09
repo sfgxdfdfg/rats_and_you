f7bc8c93f5f6188420a4032aec919aeac876abf2
Mark_developer[HUN] <97959148+sfgxdfdfg@users.noreply.github.com>
2025-02-09 23:37:57 +0100

    changed .expect message

--- src/items.rs
+++ src/items.rs
@@ -5,13 +5,13 @@ pub fn item_found(progress: i32, diff: i32, rhp: i32, rdamage: i32, php: i32, pd
     println!("You found an axe!");
     println!("Do you want to pick it up?");
     let mut choice = String::new();
-    stdin().read_line(&mut choice).expect("Nem jo!");
+    stdin().read_line(&mut choice).expect("Not good!");
     match choice.trim() {
         "y" => {
             println!("You picked it up!");
             let picked_up = true;
             going_forward(progress, diff, rhp, rdamage, php, pd, picked_up);
         },
-        _ => println!("Nem jo!")
+        _ => println!("Not good!")
     }
 }
